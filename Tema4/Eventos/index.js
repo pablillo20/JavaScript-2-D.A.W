a4bceb2765a2802ca6d47103a802a335f5d90134
Pablo Rubio <rubiopablo577@gmail.com>
2024-10-24 10:09:22 +0200

    Commit hola

--- Tema4/Eventos/index.js
+++ Tema4/Eventos/index.js
@@ -6,6 +6,7 @@ btn[0].addEventListener("mouseout", () => document.body.style.background="red");
 btn[1].addEventListener("click", () => document.body.style.background="blue");
 btn[1].addEventListener("click", () => btn[0].removeEventListener("mouseout"));
 btn[1].addEventListener("click", () => btn[0].removeEventListener("mouseover"));
+hola
 
 
 
