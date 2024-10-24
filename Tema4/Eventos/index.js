ca6bd9e7494e596fadaea20052c9432073b118f7
Pablo Rubio <rubiopablo577@gmail.com>
2024-10-24 10:20:25 +0200

    Actividad1

--- Tema4/Eventos/index.js
+++ Tema4/Eventos/index.js
@@ -6,7 +6,6 @@ btn[0].addEventListener("mouseout", () => document.body.style.background="red");
 btn[1].addEventListener("click", () => document.body.style.background="blue");
 btn[1].addEventListener("click", () => btn[0].removeEventListener("mouseout"));
 btn[1].addEventListener("click", () => btn[0].removeEventListener("mouseover"));
-hola
 
 
 
