4b07385d6029d3df1f070d03ef8fc3d9641a2518
Pablo Rubio <pabloruno4@gmail.com>
2024-10-25 12:05:55 +0200

    Actualizacion Ejercicio 4

--- Tema4/WS1/Ejercicio4/index.js
+++ Tema4/WS1/Ejercicio4/index.js
@@ -5,44 +5,29 @@ document.write('<table style="border-collapse: collapse;">');
 for (let i = 0; i < 100; i++) {
     document.write('<tr>'); // Iniciar una fila
     for (let j = 0; j < 100; j++) {
-        // Añadir una celda con bordes en línea y el evento 'onmouseover' para pintar
-        document.write('<td style="border: 1px solid black; width: 10px; height: 10px;" onmouseover="pintarCelda(event)"></td>');
+        // Añadir una celda con bordes en línea
+        document.write('<td style="border: 1px solid black; width: 10px; height: 10px;></td>');
     }
     document.write('</tr>'); // Cerrar la fila
 }
 
 document.write('</table>');
 
-// Crear variables para las teclas
-let control = false;
-let shift = false;
+let listaTD = document.getElementsByTagName("td");
 
-// Saber si estamos pulsando la tecla
-document.addEventListener("keydown", function (event) {
-    if (event.key === "Control") {
-        control = true;
-    }
-    if (event.key === "Shift") {
-        shift = true;
-    }
-});
-
-// Saber si hemos soltado la tecla
-document.addEventListener("keyup", function (event) {
-    if (event.key === "Control") {
-        control = false;
-    }
-    if (event.key === "Shift") {
-        shift = false;
-    }
-});
+for (let celda of listaTD){
+   celda.addEventListener("mouseover",pintarCelda);
+}
 
 // Pintar celdas
 function pintarCelda(event) {
-    const celda = event.target;
-    if (control) {
-        celda.style.backgroundColor = "red"; // Pintar de rojo si Ctrl está presionado
-    } else if (shift) {
-        celda.style.backgroundColor = "blue"; // Pintar de azul si Shift está presionado
+    if (event.altKey) {
+        event.preventDefault(); // Evitar el menú contextual
+        event.stopPropagation();
+        event.target.style.backgroundColor = "white"; // Borrar color de la celda con clic derecho
+    } else if (event.ctrlKey) {
+        event.target.style.backgroundColor = "red"; // Pintar de rojo si Ctrl está presionado
+    } else if (event.shiftKey) {
+        event.target.style.backgroundColor = "blue"; // Pintar de azul si Shift está presionado
     }
 }
